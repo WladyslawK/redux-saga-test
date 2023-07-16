08c4d0732277712dc19d80186f129405282b53e3
WladyslawK <vladkurr7890@gmail.com>
2023-07-16 19:45:15 +0200

    feat: count value

--- src/Counter/index.tsx
+++ src/Counter/index.tsx
@@ -1,8 +1,18 @@
+import {useDispatch, useSelector} from "react-redux"
+import { RootStateType } from "../store/store"
+
 export const Counter = () => {
+    
+
+    const count = useSelector<RootStateType, number>(state => state.counterReducer.count);
+    
     return (
         <div>
             <h1>Counter</h1>
-            
+            <p>value: {count}</p>
+            <button>increment</button>
+            <button>decremnent</button>
+
         </div>
     )
 }
